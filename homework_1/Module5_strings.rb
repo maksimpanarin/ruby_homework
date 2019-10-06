bc98932fee2344349a5dfdfeb38a22eb5665f1cc
Work <work@Users-MacBook-Pro.local>
2019-10-06 22:29:17 +0300

    Module5_strings added

---
+++ homework_1/Module5_strings.rb
@@ -0,0 +1,13 @@
+# `strings`
+# 18. Create some long string and find index of substring in this string
+
+# 19. Create string and convert to uppercase
+string = "some random string"
+uppercaseString = string.upcase
+puts uppercaseString
+
+# 20. Create string and convert to lowercase
+string = "Some Random String"
+lowercaseString = string.downcase
+puts lowercaseString
+# 21. Create string with upper and lower case letters and convert lowercase to upper case letter and vice versa
