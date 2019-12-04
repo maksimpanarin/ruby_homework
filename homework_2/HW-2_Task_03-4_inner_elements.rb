b70d93ac8348aa10342c5154769ccf983e96976d
Work <work@Users-MBP.lan>
2019-12-04 22:11:53 +0200

    HW-2/Task 03 - Completed

---
+++ homework_2/HW-2_Task_03-4_inner_elements.rb
@@ -0,0 +1,12 @@
+# 3. User has list with 10 elements. Print only 4 inner elements using list slices. [docs](https://apidock.com/ruby/v1_9_3_392/Array/slice)
+
+elements = [0,1,2,3,4,5,6,7,8,9]
+
+# Option #1
+p elements[3..6]
+
+#Option #2
+length = elements.length
+start_point = (length - 4)/2
+end_point = -(start_point + 1)
+p elements[start_point..end_point]
