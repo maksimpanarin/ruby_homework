e3b20382832cf1f7dbc8e567990408613216eaef
Work <work@Users-MBP.lan>
2019-12-25 21:32:40 +0200

    HW-2/Task 08 - Completed

---
+++ homework_2/HW-2_Task_08-Struct_data.rb
@@ -0,0 +1,11 @@
+# 8. Create Struct data structure for describing next entities: Deposit, Loan. 
+# Deposit should contain next fields: balance, id, date_created, date_closed, loans. 
+# Loan should contain next fields: id outstanding date_created, date_closed
+
+Deposit = Struct.new(:balance, :id, :date_created, :date_closed, :loans)
+
+Loan = Struct.new(:id, :outstanding, :date_created, :date_closed)
+
+p Deposit.new(20000, 1, "2001-02-03", "2001-12-03", 111111)
+
+p Loan.new(2, 50000, "2001-02-03", "2001-12-03")
